8299d477dc8e9213d5cf4afd530ab4dc8ded297a
zerocoolvishal <zerocoolvishal@gmail.com>
2018-08-19 11:09:03 +0530

    gallery moved to components and loading files moved to components/load/

--- components/head.php
+++ components/head.php
@@ -3,11 +3,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="">
 
-    <title>Album example for Bootstrap</title>
+    <title>prox</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
+    
   </head>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!--import CSS-->
-    <?php include 'load_css.html' ?>
+    <?php include 'components/load/load_css.html' ?>
     <style>
        .section{
          padding-bottom:100px;
@@ -29,25 +29,25 @@
 
       <div class="section">
         <h2>Bolliwood</h2>
-        <?php include "gallery.php"?>
+        <?php include "components/gallery.php"?>
         <button type="button" class="btn btn-default float-right">Show All</button>
       </div>
 
       <div class="section">
       <h2>Holliwood</h2>
-      <?php include "gallery.php"?>
+      <?php include "components/gallery.php"?>
       <button type="button" class="btn btn-default float-right">Show All</button>
       </div>
 
       <div class="section">
       <h2>Netflix</h2>
-      <?php include "gallery.php"?>
+      <?php include "components/gallery.php"?>
       <button type="button" class="btn btn-default float-right">Show All</button>
       </div>
 
       <div class="section">
       <h2>Other Popular</h2>
-      <?php include "gallery.php"?>
+      <?php include "components/gallery.php"?>
       <button type="button" class="btn btn-default float-right">Show All</button>
       </div>
 
@@ -56,6 +56,6 @@
     <?php include "components/footer.php" ?>
 
     <!--import JavaScript-->
-    <?php include 'load_js.html' ?>
+    <?php include 'components/load/load_js.html' ?>
   </body>
 </html>
